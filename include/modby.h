11f53a9fe59af9055959d2a4fff111a0dc7f0004
yboT1905 <romerotobias@hotmail.com>
2026-06-16 21:37:11 -0300

    nueva modif

--- include/modby.h
+++ include/modby.h
@@ -5,11 +5,9 @@
 #include <WiFi.h>
 #include "nanomodbus.h"
 
-// Memoria compartida
 extern uint16_t holding_registers[1000];
 extern bool coils[256];
 
-// Declaración de la función para que el main.cpp la encuentre
 void handle_modbus_client(WiFiClient* client);
 
 #endif
